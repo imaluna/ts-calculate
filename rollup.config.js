328e1eba855d1e1843c8338eb82954e32865a25b
imaluna <tracyhasacat@hotmail.com>
2024-11-24 22:08:33 +0800

    modify package.json

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,32 @@ const resolve = require('@rollup/plugin-node-resolve');
 const typescript = require('@rollup/plugin-typescript');
 const commonjs = require('@rollup/plugin-commonjs');
 const terser = require('@rollup/plugin-terser');
-
-module.exports = {
-	input: './packages/index.ts',
-	output: [
+const unpkg = process.argv.includes('--unpkg');
+const output = [];
+const plugins = [
+	resolve(),
+	commonjs(),
+	typescript({
+		tsconfig: './tsconfig.json',
+		compilerOptions: { incremental: false }
+	})
+];
+if (!unpkg) {
+	output.push(
 		{
 			dir: 'lib',
 			format: 'cjs',
 			entryFileNames: 'calculate.cjs.js',
 			sourcemap: false
 		},
+		// Keep the bundle as an ES module file
 		{
 			dir: 'lib',
 			format: 'esm',
 			entryFileNames: 'calculate.esm.js',
 			sourcemap: false
 		},
+		// Universal Module Definition, works as amd, cjs and iife all in one
 		{
 			dir: 'lib',
 			format: 'umd',
@@ -25,22 +35,30 @@ module.exports = {
 			name: 'calculatejs',
 			sourcemap: false
 		}
-	],
-	plugins: [
-		resolve(),
-		commonjs(),
-		typescript({
-			tsconfig: './tsconfig.json',
-			compilerOptions: { incremental: false }
-		}),
+	);
+	plugins.push(
 		terser({
 			compress: {
 				pure_getters: true,
 				unsafe: true,
 				unsafe_comps: true,
-				// drop_console: true,
+				drop_console: true,
 				drop_debugger: true
 			}
 		})
-	]
+	);
+} else {
+	output.push({
+		dir: 'lib',
+		format: 'umd',
+		entryFileNames: 'calculate.js',
+		name: 'calculatejs',
+		sourcemap: false
+	});
+}
+
+module.exports = {
+	input: './packages/index.ts',
+	output,
+	plugins
 };
